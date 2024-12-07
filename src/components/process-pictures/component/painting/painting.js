9d76db87b112a8f63ce1e89ad7a445246f8c28d7
slzgx <slzgxw@163.com>
2024-12-07 12:18:50 +0800

    完成裁剪1X1

--- src/components/process-pictures/component/painting/painting.js
+++ src/components/process-pictures/component/painting/painting.js
@@ -1,45 +1,35 @@
 import {message} from "ant-design-vue";
 import {usePsStore} from "~/stores/ps.js";
-import {Canvas, FabricImage,Line,FabricText} from "fabric";
+import {Canvas, FabricImage} from "fabric";
 
 
 export async function createCanvas(el,props) {
     const psStore = usePsStore();
     psStore.FabricCanvas.value = new Canvas(el, {
-        width: props.canvasWidth, // 初始化画布宽度
-        height: props.canvasHeight, // 初始化画布高度
+        width: props.canvasWidth,
+        height: props.canvasHeight,
     });
 
     try {
-        // 异步加载图片
         psStore.FabricImage.value = await FabricImage.fromURL(props.imgUrl, {
-            crossOrigin: "anonymous", // 处理跨域问题
+            crossOrigin: "anonymous",
         });
-
-        // 获取图片原始宽高
         const imgWidth = psStore.FabricImage.value.width;
         const imgHeight = psStore.FabricImage.value.height;
-
-        // 计算缩放比例
         const scaleX = props.canvasWidth / imgWidth;
         const scaleY = props.canvasHeight / imgHeight;
-        const scale = Math.min(scaleX, scaleY) * 0.9; // 保持比例缩放,放大尺寸
-        // 设置图片的缩放比例和居中
+        const scale = Math.min(scaleX, scaleY) * 0.9; // 保持比例缩放,放大尺寸，需要按照比例时将scaleX、scaleY设置为scale
         psStore.FabricImage.value.set({
-            scaleX: scale,
-            scaleY: scale,
-            selectable: false, // 禁止选择
-            objectCaching: false, // 禁用缓存，提升缩放性能
-            originX: "center", // 设置图片原点为中心
+            scaleX: 1,
+            scaleY: 1,
+            selectable: false,
+            objectCaching: false,
+            originX: "center",
             originY: "center",
             left: props.canvasWidth  / 2,
             top: props.canvasHeight / 2,
         });
-
-        // 设置底图
         psStore.FabricCanvas.value.backgroundImage = psStore.FabricImage.value
-  
-        // psStore.FabricCanvas.value.add(psStore.FabricImage.value);
     } catch (error) {
         console.log(error)
         message.error({
