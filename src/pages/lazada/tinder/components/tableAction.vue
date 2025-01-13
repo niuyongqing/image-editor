0f5d488102baa227a4cf21227f7e2bba621f89f9
xuminjun <1786143676@qq.com>
2025-01-13 11:06:45 +0800

    lazada 数据搬家页面

---
+++ src/pages/lazada/tinder/components/tableAction.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="my-12px">
+        <div class="w-full flex justify-between items-center">
+            <div class="pl-2">
+                <a-button type="primary" style="height: 32px;  margin-left: 10px;" @click="batchClaim">
+                    批量认领
+                </a-button>
+            </div>
+
+            <div class="pl-2">
+                <a-button type="primary" style="height: 32px;  margin-left: 10px;" @click="syncProduct">
+                    同步产品
+                </a-button>
+            </div>
+        </div>
+    </div>
+    <!-- 批量认领弹窗 -->
+    <ClaimModal ref="claimModalRef"></ClaimModal>
+    <BaseProgess v-model:open="processVisible" v-model:percent="percent" @submit="syncProductProgess" title="同步产品">
+    </BaseProgess>
+</template>
+
+<script setup>
+import BaseModal from '@/components/baseModal/BaseModal.vue';
+import BaseProgess from '@/components/baseProgess/BaseProgess.vue';
+import ClaimModal from './claimModal.vue';
+import { SettingOutlined } from "@ant-design/icons-vue";
+
+const emits = defineEmits(["success"]);
+const claimModalEl = useTemplateRef("claimModalRef");
+const processVisible = ref(false);//同步弹窗
+const percent = ref(0); //同步进度条
+
+//  批量认领
+const batchClaim = () => {
+    claimModalEl.value.open();
+};
+const syncProduct = () => {
+    processVisible.value = true;
+}
+
+const syncProductProgess = () => {
+    emits('success');
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/pages/lazada/tinder/components/task.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a-card class="mt-2.5 text-left" title="数据搬家任务流程图">
+            <a-steps v-model:current="current" :items="stepsList"></a-steps>
+        </a-card>
+    </div>
+</template>
+
+<script setup>
+const current = defineModel('current', { type: Number, default: 0 });
+const stepsList = [
+    {
+        title: '同步产品',
+        disabled: false,
+    },
+    {
+        title: '认领产品',
+        disabled: true,
+    },
+    {
+        title: '选择店铺',
+        disabled: true,
+    },
+    {
+        title: '进入采集箱',
+        disabled: true,
+    },
+]
+</script>
+
+<style lang="less" scoped></style>
