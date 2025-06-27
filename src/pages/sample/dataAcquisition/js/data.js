4bc984f5e2f2ef27911db3c260d771035b039470
王宏达 <webwanghongda@163.com>
2025-06-27 16:21:35 +0800

    feat 上线前更改

---
+++ src/pages/sample/dataAcquisition/js/data.js
@@ -0,0 +1,34 @@
+export const platformList = [
+  // {
+  //   account: "shopee",
+  //   simpleName: "Shopee"
+  // },
+  {
+    account: "alibaba",
+    simpleName: "阿里巴巴国际站"
+  },
+  // {
+  //   account: "amazaon",
+  //   simpleName: "Amazon"
+  // },
+  // {
+  //   account: "taobao",
+  //   simpleName: "淘宝"
+  // },
+  {
+    account: "tmall",
+    simpleName: "天猫"
+  },
+  {
+    account: "aliexpress",
+    simpleName: "速卖通"
+  },
+  // {
+  //   account: "lazada",
+  //   simpleName: "Lazada"
+  // },
+  {
+    account: "wb",
+    simpleName: "野莓（Wildberries）"
+  },
+]
