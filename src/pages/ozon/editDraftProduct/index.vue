9e21c8e4bea8272933b840aeb5b2589a7a42d277
林琪 <1799365444@qq.com>
2025-07-29 08:52:17 +0800

    fix/隐藏采集箱编辑按钮

--- src/pages/ozon/editDraftProduct/index.vue
+++ src/pages/ozon/editDraftProduct/index.vue
@@ -5,8 +5,8 @@
                 <div>
                     <a-breadcrumb separator=">">
                         <a-breadcrumb-item>Ozon</a-breadcrumb-item>
-                        <a-breadcrumb-item>在线产品</a-breadcrumb-item>
-                        <a-breadcrumb-item>编辑产品 </a-breadcrumb-item>
+                        <a-breadcrumb-item>采集箱</a-breadcrumb-item>
+                        <a-breadcrumb-item>编辑产品</a-breadcrumb-item>
                     </a-breadcrumb>
                 </div>
 
@@ -28,8 +28,8 @@
                                 </a-menu>
                             </template>
 </a-dropdown>-->
-                        <a-button type="default" style="height: 32px; background-color: #F5F5F5; color: #434649ff;"
-                            :loading="tempLoading" @click="showTempModal">存为模板</a-button>
+                        <!-- <a-button type="default" style="height: 32px; background-color: #F5F5F5; color: #434649ff;"
+                            :loading="tempLoading" @click="showTempModal">存为模板</a-button> -->
                         <a-dropdown>
                             <a-button style="height: 32px; background-color: #FF8345; color: #fff;">
                                 引用产品
@@ -40,12 +40,12 @@
                                     <a-menu-item :key="1">
                                         引用现有产品
                                     </a-menu-item>
-                                    <a-menu-item :key="2">
+                                    <!-- <a-menu-item :key="2">
                                         引用产品模板
                                     </a-menu-item>
                                     <a-menu-item :key="3">
                                         引用ERP产品
-                                    </a-menu-item>
+                                    </a-menu-item> -->
                                 </a-menu>
                             </template>
                         </a-dropdown>
@@ -103,8 +103,8 @@
                     <a-space>
                         <!-- <a-button @click="onSubmit(2)"
                             style="height: 32px; background-color: #F5F5F5; color: #434649ff;">一键翻译</a-button> -->
-                        <a-button type="default" style="height: 32px; background-color: #F5F5F5; color: #434649ff;"
-                            @click="showTempModal">存为模板</a-button>
+                        <!-- <a-button type="default" style="height: 32px; background-color: #F5F5F5; color: #434649ff;"
+                            @click="showTempModal">存为模板</a-button> -->
                         <a-dropdown>
                             <a-button style="height: 32px; background-color: #FF8345; color: #fff;">
                                 引用产品
@@ -115,12 +115,12 @@
                                     <a-menu-item :key="1">
                                         引用现有产品
                                     </a-menu-item>
-                                    <a-menu-item :key="2">
+                                    <!-- <a-menu-item :key="2">
                                         引用产品模板
                                     </a-menu-item>
                                     <a-menu-item :key="3">
                                         引用ERP产品
-                                    </a-menu-item>
+                                    </a-menu-item> -->
                                 </a-menu>
                             </template>
                         </a-dropdown>
