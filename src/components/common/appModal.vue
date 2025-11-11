6abba48ffe61a364ddf4018fb3905b7c2fc72538
王宏达 <webwanghongda@163.com>
2025-11-11 14:40:56 +0800

    feat 公共组件修改

--- src/components/common/appModal.vue
+++ src/components/common/appModal.vue
@@ -1,17 +1,14 @@
 <template>
-  <a-modal
-    v-model:open="internalOpen"
-    :title="title"
-    :width="width"
-    :maskClosable="false"
+  <a-modal 
+    v-model:open="internalOpen" 
+    :title="title" 
+    :width="width" 
+    :maskClosable="false" 
     :keyboard="false"
     v-bind="$attrs"
   >
     <!-- 默认插槽，用于放置弹窗的主要内容 -->
-    <div
-      class="appModalContent"
-      :style="{ 'maxHeight': contentHeight }"
-    >
+    <div class="appModalContent" :style="{ 'maxHeight': contentHeight }">
       <slot name="appContent"></slot>
     </div>
 
@@ -30,7 +27,7 @@
 import { ref, watch } from 'vue';
 
 const props = defineProps({
-  modelValue: {
+  openValue: {
     type: Boolean,
     default: false,
   },
@@ -51,17 +48,17 @@ const props = defineProps({
 });
 
 // 定义emit事件
-const emit = defineEmits(['update:modelValue', 'open', 'close', 'cancel', 'ok']);
-const internalOpen = ref(props.modelValue);
-watch(() => props.modelValue, newVal => {
+const emit = defineEmits(['update:openValue', 'open', 'close', 'cancel', 'ok']);
+const internalOpen = ref(props.openValue);
+watch(() => props.openValue, newVal => {
   internalOpen.value = newVal;
 });
 
 // 监听内部open变化，同步到外部
 watch(() => internalOpen.value, (newVal, oldVal) => {
-  emit('update:modelValue', newVal);
+  emit('update:openValue', newVal);
   if (newVal) {
-    emit('open')
+    emit('open');
   }
   if (!newVal) {
     emit('close');
@@ -76,11 +73,11 @@ const closeModal = () => {
   internalOpen.value = false;
 };
 const handleCustomCancel = () => {
-  emit('cancel')
-}
+  emit('cancel');
+};
 const handleCustomOk = () => {
-  emit('ok')
-}
+  emit('ok');
+};
 // 定义暴露的方法
 defineExpose({
   openModal,
