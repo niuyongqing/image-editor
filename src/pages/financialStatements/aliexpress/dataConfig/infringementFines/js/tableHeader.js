70e9a81bd7675ab26543057e8b123679ff9abe0e
程晓龙 <842081302@qq.com>
2025-11-18 18:15:12 +0800

    版本管理

---
+++ src/pages/financialStatements/aliexpress/dataConfig/infringementFines/js/tableHeader.js
@@ -0,0 +1,52 @@
+// tableHeader.js
+export default  [
+    {
+        title: '月份',
+        key: 'month',
+        dataIndex: 'month',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '店铺',
+        key: 'simpleName',
+        dataIndex: 'simpleName',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '币种',
+        key: 'currency',
+        dataIndex: 'currency',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '美元金额',
+        key: 'usdMoney',
+        dataIndex: 'usdMoney',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '人名币金额',
+        key: 'cnyMoney',
+        dataIndex: 'cnyMoney',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '操作人',
+        key: 'userName',
+        dataIndex: 'userName',
+        align: 'center',
+        sorter: false,
+    },
+    {
+        title: '操作时间',
+        key: 'userName',
+        dataIndex: 'userName',
+        align: 'center',
+        sorter: false,
+    },
+];
