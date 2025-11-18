907a57b457b49ec32d095c3d15568c8248845a7b
程晓龙 <842081302@qq.com>
2025-11-18 12:04:56 +0800

    时间戳

---
+++ src/pages/financialStatements/aliexpress/dataConfig/erpRenew/index.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+<!--    搜索区域-->
+    <app-table-form :reset-set-menu="resetSetMenu" v-model:formData="searchForm" @onSubmit="onSubmit">
+      <template #formItemBox>
+        <a-form-item label="月份" name="month">
+          <a-range-picker v-model:value="searchForm.month" picker="month" :bordered="true" format="YYYY-MM" value-format="YYYY-MM"/>
+        </a-form-item>
+        <a-form-item label="店铺" name="name">
+          <a-select v-model:value="searchForm.name" mode="multiple" show-search allowClear :maxTagCount="1" placeholder="请输入店铺">
+            <a-select-option v-for="shop in shopOptions" :key="shop" :value="shop">
+              {{ shop }}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+      </template>
+    </app-table-form>
+
+<!--    表格区域-->
+    <app-table-box :reset-set-menu="resetSetMenu" :table-header="tableHeader" :data-source="tableData" :scroll="{x: 1800,y: tableHeight}" row-key="id" :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange}" @change="tableDataChange">
+<!--      按钮-->
+      <template #leftTool>
+        <a-button type="primary" danger><DeleteOutlined />批量删除</a-button>
+        <a-button type="primary" @click="importModel = true"><upload-outlined />导入新增</a-button>
+        <a-button type="primary"><VerticalAlignBottomOutlined />导出</a-button>
+      </template>
+
+      <template #bodyCell="{ column, record, index }">
+        <div v-if="column.dataIndex === 'netProfit'">
+          {{ timeFormats.full(record.netProfit) }}
+        </div>
+      </template>
+<!--      操作区-->
+      <template #options="{ record, column }">
+        <a-button type="text" danger>删除</a-button>
+      </template>
+<!--      分页器区域-->
+      <template #pagination>
+        <pagination v-model:current="tableParms.pageNum" v-model:pageSize="tableParms.pageSize" :total="tableParms.total" @pageNumChange="pageNumChange" @pageSizeChange="pageSizeChange"></pagination>
+      </template>
+    </app-table-box>
+
+
+<!--    导入新增弹框-->
+    <ImportModal v-model:visible="importModel"/>
+  </div>
+</template>
+
+<script setup>
+/*                     ERP续费                  */
+defineOptions({ name: 'erpRenew' });
+import { ref, reactive, defineAsyncComponent, onMounted, computed } from 'vue';
+import { UploadOutlined,VerticalAlignBottomOutlined,DeleteOutlined } from '@ant-design/icons-vue';
+import { timeFormats } from "~/utils/dateUtils.js";
+import tableHeader from '@/pages/financialStatements/aliexpress/dataConfig/erpRenew/js/tableHeader.js';
+
+// 异步加载组件
+const appTableBox = defineAsyncComponent(() => import('@/components/common/appTableBox.vue'));
+const appTableForm = defineAsyncComponent(() => import('@/components/common/appTableForm.vue'));
+const pagination = defineAsyncComponent(() => import('@/components/common/appTablePagination.vue'));
+const ImportModal  = defineAsyncComponent(() => import('@/pages/financialStatements/aliexpress/dataConfig/erpRenew/common/importModel.vue'));
+
+const resetSetMenu = 'erpRenew';
+const importModel = ref(false);//导入新增弹框
+const tableData = ref([]);
+const shopOptions = ref(['店铺1', '店铺2', '店铺3']);
+const selectedRowKeys = ref([]);
+
+const tableParms = reactive({
+  pageNum: 1,
+  pageSize: 50,
+  total: 0,
+});
+const searchForm = reactive({
+  pageNum: 1,
+  pageSize: 50,
+  month: null,
+  name: [],
+});
+
+onMounted(() => {
+  tableData.value = [
+    {
+      id: 1,
+      netProfit: '1763436448158',
+      sourceProfit: '17.36',
+      shopProfitRate: '120.25',
+      actualProfit: '',
+      orderAmount: '102.21',
+      refundAmount: '1705386625000 '
+    },
+    {
+      id: 2,
+      netProfit: '1763436448158',
+      sourceProfit: '',
+      shopProfitRate: '',
+      actualProfit: '90.56',
+      orderAmount: '102.21',
+      refundAmount: '21.36'
+    },
+    {
+      id: 3,
+      netProfit: '1763436448158',
+      sourceProfit: '',
+      shopProfitRate: '',
+      actualProfit: '90.56',
+      orderAmount: '102.21',
+      refundAmount: '21.36'
+    },
+    {
+      id: 4,
+      netProfit: '1763436448158',
+      sourceProfit: '17.36',
+      shopProfitRate: '120.25',
+      actualProfit: '',
+      orderAmount: '102.21',
+      refundAmount: '21.36'
+    },
+    {
+      id: 5,
+      netProfit: '1763436448158',
+      sourceProfit: '17.36',
+      shopProfitRate: '120.25',
+      actualProfit: '',
+      orderAmount: '102.21',
+      refundAmount: '21.36'
+    }
+  ];
+});
+
+//进入页面计算表格高度
+const tableHeight = computed(() => {
+  return window.innerHeight - 320
+})
+
+//查询回调
+const onSubmit = (e) => {
+  console.log(searchForm);
+};
+
+//表格排序操作
+const tableDataChange = (pagination, filters, sorter) => {
+  if ( !sorter ){
+    return false;
+  }
+  console.log(sorter);
+};
+
+//页数回调
+const pageNumChange = (val) =>{
+  console.log(val);
+}
+
+//页数大小回调
+const pageSizeChange = (val) =>{
+  console.log(val);
+}
+
+const onSelectChange = (selectedKeys, selectedRows) =>{
+  selectedRowKeys.value = selectedKeys;
+  console.log(selectedRowKeys);
+}
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/financialStatements/aliexpress/dataConfig/erpRenew/js/tableHeader.js
@@ -0,0 +1,45 @@
+// tableHeader.js
+export default  [
+    {
+        title: '月份',
+        key: 'netProfit',
+        dataIndex: 'netProfit',
+        align: 'center',
+        sorter: true,
+    },
+    {
+        title: '店铺',
+        key: 'sourceProfit',
+        dataIndex: 'sourceProfit',
+        align: 'center',
+        sorter: true,
+    },
+    {
+        title: '币种',
+        key: 'shopProfitRate',
+        dataIndex: 'shopProfitRate',
+        align: 'center',
+        sorter: true,
+    },
+    {
+        title: '今金额',
+        key: 'actualProfit',
+        dataIndex: 'actualProfit',
+        align: 'center',
+        sorter: true,
+    },
+    {
+        title: '创建人',
+        key: 'orderAmount',
+        dataIndex: 'orderAmount',
+        align: 'center',
+        sorter: true,
+    },
+    {
+        title: '创建时间',
+        key: 'refundAmount',
+        dataIndex: 'refundAmount',
+        align: 'center',
+        sorter: true,
+    },
+];
