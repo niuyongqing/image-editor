e1f2c617630c6d09c7db49c08e4854633c69d489
王宏达 <webwanghongda@163.com>
2025-11-18 10:02:45 +0800

    feat 优化

--- src/pages/financialStatements/common/data.js
+++ src/pages/financialStatements/common/data.js
@@ -1,50 +1,50 @@
 export const monthList = [
   {
-      "value": "01",
-      "label": "一月"
+    value: '01',
+    label: '一月',
   },
   {
-      "value": "02",
-      "label": "二月"
+    value: '02',
+    label: '二月',
   },
   {
-      "value": "03",
-      "label": "三月"
+    value: '03',
+    label: '三月',
   },
   {
-      "value": "04",
-      "label": "四月"
+    value: '04',
+    label: '四月',
   },
   {
-      "value": "05",
-      "label": "五月"
+    value: '05',
+    label: '五月',
   },
   {
-      "value": "06",
-      "label": "六月"
+    value: '06',
+    label: '六月',
   },
   {
-      "value": "07",
-      "label": "七月"
+    value: '07',
+    label: '七月',
   },
   {
-      "value": "08",
-      "label": "八月"
+    value: '08',
+    label: '八月',
   },
   {
-      "value": "09",
-      "label": "九月"
+    value: '09',
+    label: '九月',
   },
   {
-      "value": "10",
-      "label": "十月"
+    value: '10',
+    label: '十月',
   },
   {
-      "value": "11",
-      "label": "十一月"
+    value: '11',
+    label: '十一月',
   },
   {
-      "value": "12",
-      "label": "十二月"
-  }
+    value: '12',
+    label: '十二月',
+  },
 ];
