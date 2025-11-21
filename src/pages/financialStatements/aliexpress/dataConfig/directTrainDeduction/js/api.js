df1332764fdd52fa69151ca2c776d241396d2017
程晓龙 <842081302@qq.com>
2025-11-21 08:35:38 +0800

    直通车扣费

---
+++ src/pages/financialStatements/aliexpress/dataConfig/directTrainDeduction/js/api.js
@@ -0,0 +1,24 @@
+//用户列表
+export function userList(params) {
+    return usePost('/xinzhan-system/user/by-dept-name', params)
+}
+
+//列表接口
+export function directExpressFeeList(params) {
+    return usePost('/report-aliexpress/aliexpress/DirectExpressFee/list', params)
+}
+
+//导出文件
+export function exportTemplate(params) {
+    return usePost('/report-aliexpress/aliexpress/DirectExpressFee/downloadTemplate', params)
+}
+
+//批量删除
+export function batchDelete(params) {
+    return usePost('/report-aliexpress/aliexpress/DirectExpressFee/batchDelete', params)
+}
+
+//导出列表数据
+export function exportEprList(params) {
+    return usePost('/report-aliexpress/aliexpress/DirectExpressFee/export', params)
+}
